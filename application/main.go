cc19dfad76ac44df6ddd3d4a638ff0494c1c8535
Kunal Malhotra <me@knrmalhotra.com>
2023-05-28 21:07:00 +0200

    Application code

---
+++ application/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func post(body string) string {
+	var url string = "https://polygon-rpc.com/"
+	var httpMethod string = "POST"
+	var requestBody = []byte(body)
+	request, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	responseStatus := string(response.Status)
+	responseBody, _ := io.ReadAll(response.Body)
+	return responseStatus + "\n" + string(responseBody)
+}
+
+func proxyServer(w http.ResponseWriter, r *http.Request) {
+	requestBodyOne := []byte(`{
+		"jsonrpc": "2.0",
+		"method": "eth_blockNumber",
+		"id": 2
+	  }`)
+	requestBodyTwo := []byte(`{
+		"jsonrpc": "2.0",
+		"method": "eth_getBlockByNumber",
+		"params": [
+		  "0x134e82a",
+		  true
+		],
+		"id": 2
+	  }`)
+	fmt.Println(requestBodyOne)
+	fmt.Println(requestBodyTwo)
+}
+
+func main() {
+	http.HandleFunc("/", proxyServer)
+	http.ListenAndServe(":3000", nil)
+}
